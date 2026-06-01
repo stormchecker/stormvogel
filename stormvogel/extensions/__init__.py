721d0cf1507712c3c42bbf5ba69fe6e28d1f86f5
Sebastian Junges <sebastian.junges@ru.nl>
2026-06-01 16:08:07 +0200

    fix

---
+++ stormvogel/extensions/__init__.py
@@ -0,0 +1,14 @@
+try:
+    from stormvogel.extensions.gifs import *  # NOQA
+except ImportError:
+    pass
+
+try:
+    from stormvogel.extensions.gym_grid import *  # NOQA
+except ImportError:
+    pass
+
+try:
+    from stormvogel.extensions.gym_sampling import *  # NOQA
+except ImportError:
+    pass
